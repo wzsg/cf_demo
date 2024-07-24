cfa89378842cbe9dc237778abe34593cf6209e5b
wzggsg <wzggsg@yahoo.com>
2024-07-24 17:25:35 +0700

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let headersOutput = '';
+  for (let header in req.headers) {
+    headersOutput += `${header}: ${req.headers[header]}<br>`;
+  }
+
+  res.render('index', { title: 'Express', message: headersOutput });
 });
 
 module.exports = router;
